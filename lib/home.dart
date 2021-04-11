a37eefc1e692fbd567803fe96a49707f4acd302e
FayazDheen <fayazdheen786@gmail.com>
2021-04-11 23:52:53 +0530

    updated

--- lib/home.dart
+++ lib/home.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:firebase_database/firebase_database.dart';
