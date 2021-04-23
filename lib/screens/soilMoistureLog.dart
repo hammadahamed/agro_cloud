23fb4ae188872634d98cda091cd99886646fea55
FayazDheen <fayazdheen786@gmail.com>
2021-04-23 14:44:13 +0530

    soil moisture page update

--- lib/screens/soilMoistureLog.dart
+++ lib/screens/soilMoistureLog.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:agro_cloud/utils.Dart';
+import 'package:get/get.dart';
+
 class SoilMoistureLog extends StatefulWidget {
   @override
   _SoilMoistureLogState createState() => _SoilMoistureLogState();
@@ -7,6 +10,37 @@ class SoilMoistureLog extends StatefulWidget {
 class _SoilMoistureLogState extends State<SoilMoistureLog> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        iconTheme: IconThemeData(color: MyColors.primaryColor),
+        title: Text(
+          "Soil Moisture Logs",
+          style: TextStyle(
+            fontFamily: "NunitoSans-semibold",
+            color: Get.isDarkMode ? Colors.white : MyColors.primaryColor,
+          ),
+        ),
+        backgroundColor: Get.isDarkMode ? null : Colors.white,
+        actions: [
+          Row(
+            children: [
+              IconButton(
+                tooltip: "Download Data",
+                icon: Icon(Icons.launch_rounded),
+                onPressed: null,
+              ),
+              IconButton(
+                  tooltip: "Refresh",
+                  icon: Icon(Icons.refresh),
+                  onPressed: null),
+            ],
+          )
+        ],
+      ),
+      body: Center(
+        child: Text("No Data available"),
+      ),
+    );
   }
 }
