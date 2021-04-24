dac74c739ee5e91154e3b1dd670fb96e992eb359
hammad.r <hammad.r@zohocorp.com>
2021-04-24 15:12:59 +0530

    get data function added 3

--- lib/controller/data_controller.dart
+++ lib/controller/data_controller.dart
@@ -11,7 +11,7 @@ class DataController extends GetxController {
   var temperature = [].obs;
   var allRow = [].obs;
 
-// init methods
+// init methods is this
   void onInit() {
     this.fbInitializer();
     super.onInit();
