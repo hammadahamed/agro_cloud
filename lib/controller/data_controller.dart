a45f33cedd435162b89fa2f17afe7981a20f9d11
FayazDheen <fayazdheen786@gmail.com>
2021-04-24 15:12:55 +0530

    update

--- lib/controller/data_controller.dart
+++ lib/controller/data_controller.dart
@@ -1,80 +1,6 @@
 import 'package:firebase_database/firebase_database.dart';
-<<<<<<< HEAD
-import 'package:flutter/material.dart';
-=======
->>>>>>> 4a41b8577b53f75a57188c1d38ad8d3159977a57
 import 'package:get/get.dart';
-import 'package:intl/intl.dart';
 
-class DataController extends GetxController {
-  
-  final fb = FirebaseDatabase.instance;
-  // BuildContext context;
-  bool allDataLoading = false;
-  RxList<String> humidity;
-  RxList<String> temperature;
-  RxList<String> time ;
-  RxList<String> date ;
-  RxList<String> dateTime;
-  RxList<DataRow> allRow;
-
-
-  allData({context}) async {
-
-    time.clear();
-    humidity.clear();
-    DateTime frmt;
-    String t;
-    final ref = fb.reference();
-    await ref.child("DataArray").once().then((DataSnapshot data) {
-      print("--------------------- DATA ----------------------");
-      // print(data.toString());
-      print("--------------------- DATA ----------------------");
-      // DATE
-      data.value["DateTime"].forEach((key, value) => {
-            dateTime = value.split(RegExp(r"[T,+]")),
-            frmt = new DateFormat("yyyy-MM-dd HH:mm:ss")
-                .parse(dateTime[0] + " " + dateTime[1]),
-            t = new TimeOfDay(
-              hour: frmt.hour,
-              minute: frmt.minute,
-            ).format(context),
-            t = t.replaceFirst(" ", ":" + frmt.second.toString() + " "),
-            time.add(t),
-            date.add(dateTime[0])
-          });
-      print("humidity");
-      print(data.value["Humidity"]);
-
-      data.value["Humidity"].forEach((key, value) => {
-            humidity.add(value),
-          });
-    });
-  }
-  dataRows({type}){
-  for (var i = 0; i < time.length; i++) {
-      int j = i + 1;
-
-      if (i > time.length - 50 && i < time.length) {
-        allRow.add(
-          DataRow(
-            selected: i % 2 == 0 ? true : false,
-            cells: <DataCell>[
-              DataCell(Text("$j")),
-              DataCell(Text(date[i])),
-              DataCell(Text(time[i])),
-              DataCell(Text(humidity[i])),
-            ],
-          ),
-        );
-      }
-    }
-     return allRow;
-}
-}
-
-<<<<<<< HEAD
-=======
 class DataController extends GetxController {
   var fb;
   var fbRef;
@@ -85,7 +11,6 @@ class DataController extends GetxController {
   var temperature = [].obs;
   var allRow = [].obs;
 
-// init methods
   void onInit() {
     this.fbInitializer();
     super.onInit();
@@ -116,4 +41,3 @@ class DataController extends GetxController {
     });
   }
 }
->>>>>>> 4a41b8577b53f75a57188c1d38ad8d3159977a57
